6b0baa5a6004a3c7f4cbba46a8f12943beae844b
Liumanov Vadym <vliumanov@gmail.com>
2021-11-12 10:53:35 +0200

    small changes 002

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import '../css/Header.css'
+
 const Header = () => {
   return (
     <div className="header">
-      <img src="https://lh5.googleusercontent.com/FXUvmb1Ph0hZdiaNWOYHj5J_y54pj9d-5sjm247jxdmZ74EJ8PeJTCztyzxNyaqbzLxNPjn68JIqjfYPXisYAFKYZt-n0drUr79JjXv71Ugd-j6H_5IIdMbCIWP6NcRYBazVZEMV" alt="logo" height="60" />
+      <img src="https://lh5.googleusercontent.com/FXUvmb1Ph0hZdiaNWOYHj5J_y54pj9d-5sjm247jxdmZ74EJ8PeJTCztyzxNyaqbzLxNPjn68JIqjfYPXisYAFKYZt-n0drUr79JjXv71Ugd-j6H_5IIdMbCIWP6NcRYBazVZEMV" alt="logo" />
       <div>
 
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+import '../css/Navbar.css'
+
 const Navbar = () => {
   return (
     <nav className="nav">
