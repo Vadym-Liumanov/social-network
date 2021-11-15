cdb285cc0af2a575d6408ee276da364208295e52
Liumanov Vadym <vliumanov@gmail.com>
2021-11-15 11:10:51 +0200

    new page Dialogs has been added

---
+++ src/components/Dialogs/Dialogs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import dialogsStyles from'./Dialogs.module.css'
+
+const Dialogs = () => {
+  return (
+    <div className={dialogsStyles.content}>
+      DIALOGS
+    </div>
+  );
+}
+
+export default Dialogs
