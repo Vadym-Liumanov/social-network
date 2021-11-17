2ce10833d064585604afd47a5105f5ff73a9ed29
Liumanov Vadym <vliumanov@gmail.com>
2021-11-17 15:20:49 +0200

    Dannie iz index.js vineseni v /src/redux/state.js - polu4ili ediniy state dlya vsego App

--- src/index.js
+++ src/index.js
@@ -4,68 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const data = {
-  profilePosts: [
-    {
-      id: 1,
-      post: 'It is my first post!',
-      likesCount: 20
-    },
-    {
-      id: 2,
-      post: 'How are you?',
-      likesCount: 10
-    },
-    {
-      id: 3,
-      post: 'Hello World',
-      likesCount: 5
-    },
-    {
-      id: 4,
-      post: 'Why nobody loves me',
-      likesCount: 12
-    }
-  ],
-  messages: {
-    users: [
-      {
-        id: 1,
-        userName: 'Dima'
-      },
-      {
-        id: 2,
-        userName: 'Helen'
-      },
-      {
-        id: 3,
-        userName: 'Vovan'
-      },
-      {
-        id: 4,
-        userName: 'Alex'
-      }
-    ],
-    dialogs: [
-      {
-        id: 1,
-        dialog: 'How are you?'
-      },
-      {
-        id: 2,
-        dialog: 'I am fine.'
-      },
-      {
-        id: 3,
-        dialog: 'What are you doing'
-      },
-      {
-        id: 4,
-        dialog: 'Let\'s go to the stadium.'
-      }
-    ]
-  }
-}
+import data from './redux/state'
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/redux/state.js
@@ -0,0 +1,64 @@
+const state = {
+  profilePosts: [
+    {
+      id: 1,
+      post: 'It is my first post!',
+      likesCount: 20
+    },
+    {
+      id: 2,
+      post: 'How are you?',
+      likesCount: 10
+    },
+    {
+      id: 3,
+      post: 'Hello World',
+      likesCount: 5
+    },
+    {
+      id: 4,
+      post: 'Why nobody loves me',
+      likesCount: 12
+    }
+  ],
+  messages: {
+    users: [
+      {
+        id: 1,
+        userName: 'Dima'
+      },
+      {
+        id: 2,
+        userName: 'Helen'
+      },
+      {
+        id: 3,
+        userName: 'Vovan'
+      },
+      {
+        id: 4,
+        userName: 'Alex'
+      }
+    ],
+    dialogs: [
+      {
+        id: 1,
+        dialog: 'How are you?'
+      },
+      {
+        id: 2,
+        dialog: 'I am fine.'
+      },
+      {
+        id: 3,
+        dialog: 'What are you doing'
+      },
+      {
+        id: 4,
+        dialog: 'Let\'s go to the stadium.'
+      }
+    ]
+  }
+}
+
+export default state
