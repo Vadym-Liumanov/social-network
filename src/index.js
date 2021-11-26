0ced91d1121b2720ac57f30462e2b72024d94ea5
Liumanov Vadym <vliumanov@gmail.com>
2021-11-26 16:32:21 +0200

    small

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import store from './redux/store-redux'
 import './index.css'
 import App from './App'
 
-import { Provider } from './StoreContext'
+
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
