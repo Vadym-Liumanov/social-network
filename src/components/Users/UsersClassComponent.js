c1993704addc8a3201b6eec4642121a8e932d1ff
Liumanov Vadym <vliumanov@gmail.com>
2021-12-08 13:41:24 +0200

    Side effect - zapros userov s API - realizovan v UsersClassComponent.js pravilno - 4erez lifecycle method ComponentDidMount()

--- src/components/Users/UsersClassComponent.js
+++ src/components/Users/UsersClassComponent.js
@@ -6,17 +6,15 @@ import User from './User/User'
 import usersStyles from './Users.module.css'
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props)
-      if (this.props.usersList.length === 0) {
-        // alert('New class component was created!')
-        const apiUrl = 'https://social-network.samuraijs.com/api/1.0/users'
-        axios.get(apiUrl).then((response) => { this.props.setUsers(response.data.items) })
-      }
+
+  componentDidMount() {
+    if (this.props.usersList.length === 0) {
+      const apiUrl = 'https://social-network.samuraijs.com/api/1.0/users'
+      axios.get(apiUrl).then((response) => { this.props.setUsers(response.data.items) })
+    }
   }
 
   render() {
-
     let usersListItems = this.props.usersList.map(user => <User key={user.id} userInfo={user} followToggle={this.props.followToggle} />)
 
     return (
