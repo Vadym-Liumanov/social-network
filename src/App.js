641852418296da1987490b3e816b16bec42d38a0
Vadym Liumanov <vliumanov@gmail.com>
2022-07-27 12:31:39 +0300

    BrowserRouter was changed on HashRouter in App.js for deploying on GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom'
+import { Route, HashRouter, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { initializeAppThunk } from './redux/app-reducer'
@@ -30,7 +30,7 @@ class App extends React.Component {
     }
 
     return (
-      <BrowserRouter>
+      <HashRouter>
         <div className="app-wrapper">
           <Header />
           <Navbar />
@@ -51,7 +51,7 @@ class App extends React.Component {
             </Switch>
           </div>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 }
