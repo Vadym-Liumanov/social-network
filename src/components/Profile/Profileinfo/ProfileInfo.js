892de164756632c4a9878d61c544e46b6e4108ca
Vadym Liumanov <vliumanov@gmail.com>
2022-07-29 13:00:21 +0300

    Stil working at Profileinfo.js. At step of changing photo 001

--- src/components/Profile/Profileinfo/ProfileInfo.js
+++ src/components/Profile/Profileinfo/ProfileInfo.js
@@ -6,21 +6,32 @@ import userImage from '../../../assets/images/userImage.jpg'
 import ProfileStatus from './ProfileStatusWithHooks'
 
 
-const ProfileInfo = ({ fullName, ...props }) => {
+const ProfileInfo = ({ fullName, photos, isOwner, userStatus, myStatus, updateMyStatus, aboutMe,
+  contacts, lookingForAJob, lookingForAJobDescription }) => {
   // debugger
+
+  const notSpecified = 'Not specified'
+
+  const onFileSelect = (e) => {
+    const temp = e.target.files[0]
+    console.log(temp)
+  }
 
   return (
     <div className={styles.container}>
       <section className={styles.avaNameStatus}>
+        <div className={styles.avaContainer}>
+          <img src={photos.small ? photos.small : userImage} alt="userAvatar" className={styles.avatar} />
+        </div>
         <div>
-          <img src={props.photos.small ? props.photos.small : userImage} alt="userAvatar" className={styles.avatar} />
+          <input type="file" onChange={onFileSelect} />
         </div>
         <div className={styles.fullName}><span>{fullName}</span></div>
         <div className={styles.status}>
           <div className={styles.status__title}>Status:</div>
           <div className={styles.status__text}>
-            {!props.isOwner && (<>{props.userStatus || 'User have no status!'}</>)}
-            {props.isOwner && (<ProfileStatus status={props.myStatus} updateMyStatus={props.updateMyStatus} />)}
+            {!isOwner && (<>{userStatus || 'User have no status!'}</>)}
+            {isOwner && (<ProfileStatus status={myStatus} updateMyStatus={updateMyStatus} />)}
           </div>
         </div>
       </section>
@@ -28,7 +39,16 @@ const ProfileInfo = ({ fullName, ...props }) => {
         <div>
           <div className={styles.aboutMe__title}>About me</div>
           <div>
-            {props.aboutMe}
+            {aboutMe || notSpecified}
+          </div>
+          <div>
+            {/* { contacts || notSpecified} */}
+          </div>
+          <div>
+            lookingForAJob: {lookingForAJob || notSpecified}
+          </div>
+          <div>
+            lookingForAJobDescription:{lookingForAJobDescription || notSpecified}
           </div>
         </div>
       </section>
