d6138e84d682f9fc5ed10e2ff17e5b98537c5554
Vadym Liumanov <vliumanov@gmail.com>
2022-08-09 10:06:14 +0300

    Typescript was added to the project. App-reducer.js was changed to app-reducer.tsx also

---
+++ src/redux/app-reducer.tsx
@@ -0,0 +1,37 @@
+import { getAuthDataThunk } from './auth-reducer'
+
+const SET_APP_INITIALIZED = 'social_network/app/SET_APP_INITIALIZED'
+
+const appInitSuccessAC = () => {
+  return { type: SET_APP_INITIALIZED }
+}
+
+export const initializeAppThunk = () => {
+  return (dispatch: any) => {
+    const promise = dispatch(getAuthDataThunk())
+    Promise.all([promise]).then(() => {
+      dispatch(appInitSuccessAC())
+    })
+  }
+}
+
+// state = state.app
+const initialState = {
+  isAppInitialized: false
+}
+
+const appReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+
+    case SET_APP_INITIALIZED:
+      return {
+        ...state,
+        isAppInitialized: true
+      }
+
+    default:
+      return state
+  }
+}
+
+export default appReducer
