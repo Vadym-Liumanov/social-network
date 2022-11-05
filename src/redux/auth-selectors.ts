6e2932369e2f4a93b123e1a6684c4cdb20c9ce11
Vadym Liumanov <vliumanov@gmail.com>
2022-11-05 18:13:04 +0300

    Hooks were aplied to the Login.tsx. Todo: enable Preloader when data is fetching or disable login button on same time

--- src/redux/auth-selectors.ts
+++ src/redux/auth-selectors.ts
@@ -1,4 +1,5 @@
 import {AppStateType} from './store-redux'
 
 export const getIsAuth = (state: AppStateType) => state.auth.isAuth
+export const getCaptchaUrl = (state: AppStateType) => state.auth.captchaUrl
 export const getAuthData = (state: AppStateType) => state.auth
