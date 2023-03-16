1ef152ba1ed5db850f58cd0c6bcbbef35411dddc
Vadym Liumanov <vliumanov@gmail.com>
2023-03-16 14:28:12 +0300

    Small updates in Preloader

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import './preloader.css'
+import styles from './Preloader.module.css'
 
 import preloader from './../../../assets/preloaders/spinning-circles.svg'
 
 const Preloader: React.FC = () => {
   return (
-    <div className="initial-app-preloader">
+    <div className={styles.appPreloader}>
       <img src={preloader} alt='preloader' />
     </div>
   )
