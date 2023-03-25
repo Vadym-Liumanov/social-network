84027e0b921c1b9b5eaedf4840780954c288904e
Vadym Liumanov <vliumanov@gmail.com>
2023-03-25 20:42:14 +0300

    ws channel was opened. Logic was applied to WSChannel component (dirty settings). Todo: separate components, devide responsibility between UI, BLL and DAL

--- src/components/ChatPage/ChatPage.tsx
+++ src/components/ChatPage/ChatPage.tsx
@@ -1,54 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import userAvatar from './../../assets/images/defaultUserImage.jpg'
 
 import styles from './ChatPage.module.css'
 
+type ChatMessageType = {
+  message: string
+  photo: string
+  userId: number
+  userName: string
+}
+
+type ChatMessagesType = ChatMessageType[]
+
+const WebSocketChannel = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
 const mockArr = [1, 2, 3, 4, 5]
 
-const Message = (props: any) => {
+// {
+//   message: "asdasd",
+//   photo:        "https://social-network.samuraijs.com/activecontent/images/users/2/user-small.jpg?v=4",
+//   userId: 2,
+//   userName: "samurai dimych"
+// }
+
+const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
   return (
     <div className={styles.message}>
 
       <img
-        src={userAvatar}
+        src={message.photo ? message.photo : userAvatar}
         className={styles.message__authorAvatar}
       />
       <span className={styles.message__authorName}>
-        {'Vasya Pupkin'}
+        {message.userName}
       </span>
 
       <p className={styles.message__text}>
-        {'Hello!'}
+        {message.message}
       </p>
     </div>
   )
 }
 
-const Messages = (props: any) => {
+const Messages: React.FC<{ messages: ChatMessagesType }> = ({ messages }) => {
   return (
     <div className={styles.messages__body}>
-      {mockArr.map((it, index) => {
-        return <Message key={index} />
+      {messages.map((message, index) => {
+        return <Message key={index} message={message} />
       })}
     </div>
   )
 }
 
 const ChatPage = (props: any) => {
+  const [messagesList, setMessagesList] = useState<ChatMessagesType>([])
+  const [message, setMessage] = useState('')
+
+  const onSendBtnClick = () => {
+    if (!message.trim()) {
+      setMessage('')
+      return
+    }
+    WebSocketChannel.send(message.trim())
+    setMessage('')
+  }
+
+  useEffect(() => {
+    WebSocketChannel.addEventListener('message', (e) => {
+      const newMessages: ChatMessagesType = JSON.parse(e.data)
+      setMessagesList((prevMessages) => [...prevMessages, ...newMessages])
+      console.log(messagesList)
+    })
+  }, [])
+
   return (
     <div className={styles.wrapper}>
 
       <div className={styles.body}>
 
         <div className={styles.messagesContainer}>
-          <Messages />
+          <Messages messages={messagesList} />
         </div>
 
         <div className={styles.formContainer}>
 
           <div className={styles.form__body}>
-            <textarea className={styles.form__textarea} />
-            <button className={styles.form__button}>
+            <textarea
+              className={styles.form__textarea}
+              onChange={(e) => { setMessage(e.target.value) }}
+              value={message}
+            />
+            <button
+              className={styles.form__button}
+              onClick={onSendBtnClick}
+            >
               Send
             </button>
             <div className={styles.form__counter}>
